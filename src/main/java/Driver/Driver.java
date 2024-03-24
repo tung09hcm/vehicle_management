ccb880e811246c6a7f44e025957b26358856e96a
tien0246 <60417827+tien0246@users.noreply.github.com>
2024-03-24 21:32:24 +0700

    check spell

--- src/main/java/Driver/Driver.java
+++ src/main/java/Driver/Driver.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 public class Driver {
     private String name;
-    private String phone_number;
+    private String phoneNumber;
     private String address;
-    private ArrayList<License> vector_of_license;
+    private ArrayList<License> license;
     private DriverStatus status;
     private ArrayList<Trip> history;
 
     public String getName() {
         return name;
     }
-    public String getPhone_number() {
-        return phone_number;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
     public String getAddress() {
         return address;
     }
     public ArrayList<License> getVector_of_license() {
-        return vector_of_license;
+        return license;
     }
     public DriverStatus getStatus() {
         return status;
@@ -32,14 +32,14 @@ public class Driver {
     public void setName(String name) {
         this.name = name;
     }
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
     public void setAddress(String address) {
         this.address = address;
     }
-    public void setVector_of_license(ArrayList<License> vector_of_license) {
-        this.vector_of_license = vector_of_license;
+    public void setVector_of_license(ArrayList<License> license) {
+        this.license = license;
     }
     public void setStatus(DriverStatus status) {
         this.status = status;
@@ -47,11 +47,11 @@ public class Driver {
     public void setHistory(ArrayList<Trip> history) {
         this.history = history;
     }
-    public Driver(String name, String phone_number, String address, ArrayList<License> vector_of_license, DriverStatus status, ArrayList<Trip> history) {
+    public Driver(String name, String phoneNumber, String address, ArrayList<License> license, DriverStatus status, ArrayList<Trip> history) {
         this.name = name;
-        this.phone_number = phone_number;
+        this.phoneNumber = phoneNumber;
         this.address = address;
-        this.vector_of_license = vector_of_license;
+        this.license = license;
         this.status = status;
         this.history = history;
     }

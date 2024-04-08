5a85f2cb4aaa845d6c4753b3cc148b1151c1f482
tung09hcm <164112205+tung09hcm@users.noreply.github.com>
2024-04-08 21:53:29 +0700

    Update TripStatus.java
    delete uneccessary status

--- src/main/java/com/management/vehicle/trip/TripStatus.java
+++ src/main/java/com/management/vehicle/trip/TripStatus.java
@@ -3,9 +3,7 @@ package com.management.vehicle.trip;
 public enum TripStatus {
     NONE("none"),
     ON_DUTY("on-duty"),
-    NOT_APPROVED("not-approved"),
-    APPROVED("approved"),
-    APPROVING("approving");
+
     private final String status;
     TripStatus(String status)
     {

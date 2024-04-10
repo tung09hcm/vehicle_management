f070f6da12074146f1a5df018833d0910935db0f
samgiahuyfsdev <126471803+samgiahuyfsdev@users.noreply.github.com>
2024-04-10 21:57:30 +0700

    check drivers' information

--- src/main/java/com/management/vehicle/gui/controller.java
+++ src/main/java/com/management/vehicle/gui/controller.java
@@ -1,5 +1,6 @@
 package com.management.vehicle.gui;
 
+import com.management.vehicle.driver.Driver;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -12,8 +13,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import com.management.vehicle.request.FireBase;
 
 import java.awt.event.ActionEvent;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -28,8 +31,14 @@ public class controller {
     @FXML
     private Button loginBtn;
 
+    FireBase connection = FireBase.getInstance();
+
     private double x = 0;
     private double y = 0;
+
+    public controller() throws Exception {
+    }
+
     public void loginAdmin() {
         Alert alert;
         try {
@@ -70,47 +79,57 @@ public class controller {
 //                });
 
             }
-            else if ((Objects.equals(username.getText(), "driver")) && (Objects.equals(password.getText(), "123456"))) {
-                alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setHeaderText(null);
-                alert.setContentText("Successfully Login!");
-                alert.showAndWait();
-                // HIDE YOUR LOGIN FORM
-                loginBtn.getScene().getWindow().hide();
+            else {
+                if (isDuplicated(isDuplicated(driverList, username.getText()))) {
 
-                // LINK YOUR DASHBOARD FORM
-                Parent root = FXMLLoader.load(getClass().getResource("/driver.fxml"));
-                Stage stage = new Stage();
-                Scene scene = new Scene(root);
-                stage.setTitle("New Window");
-                stage.setScene(scene);
-                stage.setResizable(false);
-                stage.show();
-                stage.setOnCloseRequest(event -> {
-                    event.consume();
-                    logOut(stage);
-                });
-
-//                root.setOnMousePressed((MouseEvent e) ->{
-//                    x = e.getSceneX();
-//                    y = e.getSceneY();
+                }
+//            else if ((Objects.equals(username.getText(), "driver")) && (Objects.equals(password.getText(), "123456"))) {
+//                alert = new Alert(Alert.AlertType.INFORMATION);
+//                alert.setHeaderText(null);
+//                alert.setContentText("Successfully Login!");
+//                alert.showAndWait();
+//                // HIDE YOUR LOGIN FORM
+//                loginBtn.getScene().getWindow().hide();
+//
+//                // LINK YOUR DASHBOARD FORM
+//                Parent root = FXMLLoader.load(getClass().getResource("/driver.fxml"));
+//                Stage stage = new Stage();
+//                Scene scene = new Scene(root);
+//                stage.setTitle("New Window");
+//                stage.setScene(scene);
+//                stage.setResizable(false);
+//                stage.show();
+//                stage.setOnCloseRequest(event -> {
+//                    event.consume();
+//                    logOut(stage);
 //                });
 //
-//                root.setOnMouseDragged((MouseEvent e) ->{
-//                    stage.setX(e.getScreenX() - x);
-//                    stage.setY(e.getScreenY() - y);
-//                });
-
-            }
-            else {
-                alert = new Alert(Alert.AlertType.ERROR);
-                alert.setHeaderText(null);
-                alert.setContentText("Wrong Username/Password");
-                alert.showAndWait();
+////                root.setOnMousePressed((MouseEvent e) ->{
+////                    x = e.getSceneX();
+////                    y = e.getSceneY();
+////                });
+////
+////                root.setOnMouseDragged((MouseEvent e) ->{
+////                    stage.setX(e.getScreenX() - x);
+////                    stage.setY(e.getScreenY() - y);
+////                });
+//
+//            }
+                else {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username/Password");
+                    alert.showAndWait();
+                }
             }
         }
     } catch(Exception e){e.printStackTrace();}
 
+    }
+    public Boolean isDuplicated(List<Driver> list, String ID) {
+        for (int i = 0; i < list.size(); i++)
+            if (ID.equals(list.get(i).getId())) return true;
+        return false;
     }
     public void logOut(Stage stage) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);

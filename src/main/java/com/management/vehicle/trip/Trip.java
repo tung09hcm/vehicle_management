3810c7290b565c2395b46fd95a193f651aac2a76
tung <tung.bui2004lk@hcmut.edu.vn>
2024-04-16 22:33:06 +0700

    thêm biến fuel_trip cho trip

--- src/main/java/com/management/vehicle/trip/Trip.java
+++ src/main/java/com/management/vehicle/trip/Trip.java
@@ -2,6 +2,7 @@ package com.management.vehicle.trip;
 import com.management.vehicle.request.RouteMatrix;
 import com.management.vehicle.request.struct.*;
 import com.management.vehicle.request.MapRequest;
+import com.management.vehicle.vehicle.fuel;
 
 import java.util.List;
 
@@ -33,6 +34,7 @@ public class Trip {
     public void setStatus(TripStatus status) {
         this.status = status;
     }
+    private fuel fuel_trip = fuel.NONE;
     private String beginLocation;
     private String endLocation;
     private double distanceCover;
@@ -45,6 +47,14 @@ public class Trip {
     private String driverID;
     private double cost;
     private double Revenue;
+
+    public fuel getFuel_trip() {
+        return fuel_trip;
+    }
+
+    public void setFuel_trip(fuel fuel_trip) {
+        this.fuel_trip = fuel_trip;
+    }
 
     public double getCost() {
         return cost;
